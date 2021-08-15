247be0753f4a033eaafbfe7b8114516f0c81941c
Rakesh Vardan <rakeshlearnit@gmail.com>
2021-08-15 22:35:18 +0530

    includes more tests for LT execution

---
+++ src/test/java/org/lambdatest/SeleniumTestLT.java
@@ -0,0 +1,63 @@
+package org.lambdatest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SeleniumTestLT {
+
+    public WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        try {
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            driver = new RemoteWebDriver(new URL("https://" + getUsername() + ":" + getAccessKey() + "@hub.lambdatest.com/wd/hub"), firefoxOptions);
+            driver.get("https://lambdatest.github.io/sample-todo-app/");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testScript() {
+        try {
+            driver.findElement(By.name("li1")).click();
+            driver.findElement(By.name("li2")).click();
+            driver.findElement(By.id("sampletodotext")).clear();
+            driver.findElement(By.id("sampletodotext")).sendKeys("Yey, Let's add it to list");
+            driver.findElement(By.id("addbutton")).click();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testMe() {
+        String headerText = driver.findElement(By.xpath("//h2")).getText();
+        Assert.assertEquals(headerText, "some test");
+    }
+
+    @AfterTest
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    private static String getAccessKey() {
+        return System.getenv("LT_ACCESS_KEY") != null ? System.getenv("LT_ACCESS_KEY") : "accesskey";
+    }
+
+    private static String getUsername() {
+        return System.getenv("LT_USERNAME") != null ? System.getenv("LT_USERNAME") : "username";
+    }
+}
