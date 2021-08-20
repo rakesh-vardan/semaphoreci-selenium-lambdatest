df786d805945df18263622462ecec52336154bd4
Rakesh Vardan <rakeshlearnit@gmail.com>
2021-08-20 15:12:06 +0530

    includes tests for chrome

---
+++ src/main/java/org/lambdatest/BrowserType.java
@@ -0,0 +1,12 @@
+package org.lambdatest;
+
+public enum BrowserType {
+    LOCAL_CHROME,
+    LOCAL_FIREFOX,
+    LOCAL_IE,
+    GRID_CHROME,
+    GRID_FIREFOX,
+    GRID_IE,
+    GRID_LAMBDATEST_CHROME,
+    GRID_LAMBDATEST_FIREFOX
+}
